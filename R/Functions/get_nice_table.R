1e3c60e88edadf23e40bc5fc02a3acf3ada34db6
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-02-08 11:15:12 +0100

    add rounding of final tables

--- R/Functions/get_nice_table.R
+++ R/Functions/get_nice_table.R
@@ -1,7 +1,17 @@
-get_nice_table <- function(data_source) {
-  knitr::kable(data_source,
-    digits = c(2, 4),
-    align = "l"
-  ) %>%
+get_nice_table <- function(data_source, decimal_digits = 2) {
+  data_source %>%
+    dplyr::mutate(
+      dplyr::across(
+        .cols = tidyselect::where(
+          is.numeric
+        ),
+        .fns = ~ round(.x, decimal_digits)
+      )
+    ) %>%
+    knitr::kable(
+      .,
+      digits = decimal_digits,
+      align = "l"
+    ) %>%
     return()
 }
