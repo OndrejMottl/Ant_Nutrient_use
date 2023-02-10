82df5d24c1a8601cc8bee728ec6a62a374d020ec
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-02-11 00:15:20 +0100

    udpate table S1 with new models

--- R/03_Supplementary_analyses/table_S1.r
+++ R/03_Supplementary_analyses/table_S1.r
@@ -21,40 +21,40 @@ source(
 
 
 # load models ----
-mods_ab <-
-  readr::read_rds(
-    file = here::here(
-      paste0(
-        "Data/Processed/Models/ele_trend_abundances_2023-02-02.rds"
-      )
-    )
-  )
+mod_abundnace <-
+  RUtilpol::get_latest_file(
+    file_name = "mod_abundnace",
+    dir = here::here("Data/Processed/Models/")
+  ) %>%
+  purrr::pluck("models")
 
 
 # make table ----
 table_s1 <-
-  mods_ab %>%
-  dplyr::filter(
-    mod_name == "poly_full"
-  ) %>%
-  purrr::pluck("model_details", 1) %>%
+  mod_abundnace %>%
+  dplyr::rename(model_df = df) %>%
+  dplyr::filter(best_model == TRUE) %>%
+  purrr::pluck("mod_anova", 1) %>%
   get_nice_table()
 
-# results for models selected based of AIC
-if (FALSE) {
-  table_1 <-
-    mods_ab %>%
-    dplyr::filter(
-      best_model == TRUE
-    ) %>%
-    purrr::pluck("model_details", 1) %>%
-    get_nice_table()
-}
-
-# save docx to workspace and manually copy table over to xlsx
+# save
 arsenal::write2word(
   object = table_s1,
   file = here::here(
     "Outputs/Table_S1.docx"
+  )
+)
+
+table_s1_extra <-
+  mod_abundnace %>%
+  dplyr::rename(model_df = df) %>%
+  tidyr::unnest(anova_to_null) %>%
+  dplyr::select(-c(mod, mod_anova)) %>%
+  get_nice_table()
+
+arsenal::write2word(
+  object = table_s1_extra,
+  file = here::here(
+    "Outputs/Table_s1_extra.docx"
   )
 )
