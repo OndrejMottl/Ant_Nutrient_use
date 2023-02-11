5ce3f8f13730063256f1b8de9f70dd8a3fe5273e
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-02-11 22:19:48 +0100

    utility fc to print model summary

---
+++ R/Functions/print_model_summary.R
@@ -0,0 +1,32 @@
+print_model_summary <- function(data_source) {
+  RUtilpol::output_comment("model fit:")
+  data_source %>%
+    dplyr::select(
+      mod_name, AICc, delta, r2, d2, best_model
+    ) %>%
+    print()
+
+  RUtilpol::output_comment("best model:")
+  data_source %>%
+    dplyr::filter(
+      best_model == TRUE
+    ) %>%
+    purrr::pluck("mod_name") %>%
+    print()
+
+  RUtilpol::output_comment("parameters details:")
+  data_source %>%
+    dplyr::filter(
+      best_model == TRUE
+    ) %>%
+    purrr::pluck("mod_anova") %>%
+    print()
+
+  RUtilpol::output_comment("check anova of best model to null:")
+  data_source %>%
+    dplyr::filter(
+      best_model == TRUE
+    ) %>%
+    purrr::pluck("anova_to_null") %>%
+    print()
+}
