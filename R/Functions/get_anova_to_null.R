f65f3b16653dbc065ae1729b82098db4ea3e45bd
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-02-12 21:37:29 +0100

    model details, use fc on a var not whole table

--- R/Functions/get_anova_to_null.R
+++ R/Functions/get_anova_to_null.R
@@ -1,35 +1,30 @@
 get_anova_to_null <- function(data_source, null_model) {
-  data_source %>%
-    dplyr::mutate(
-      anova_to_null = purrr::map(
-        .x = mod,
-        .f = purrr::possibly(
-          .f = ~ {
-            anova_res <-
-              anova(null_model, .x) %>%
-              as.data.frame() %>%
-              tibble::as_tibble() %>%
-              dplyr::slice(2) %>%
-              dplyr::rename_with(
-                .fn = ~ paste("aov_", .x)
-              ) %>%
-              janitor::clean_names()
+  res <- NA_real_
+  try(
+    expr = {
+      anova_res <-
+        lmtest::lrtest(null_model, data_source) %>%
+        as.data.frame() %>%
+        tibble::as_tibble() %>%
+        dplyr::slice(2) %>%
+        dplyr::rename_with(
+          .fn = ~ paste("LR_", .x)
+        ) %>%
+        janitor::clean_names()
 
-            p_value_col_name <-
-              stringr::str_subset(names(anova_res), "pr_chi")
+      p_value_col_name <-
+        stringr::str_subset(names(anova_res), "pr_chi")
 
-            anova_res %>%
-              dplyr::mutate(
-                aov_signif = insight::format_p(
-                  p = get(p_value_col_name),
-                  stars_only = TRUE
-                )
-              ) %>%
-              return()
-          },
-          otherwise = NA_real_
+      res <-
+        anova_res %>%
+        dplyr::mutate(
+          lr_signif = insight::format_p(
+            p = get(p_value_col_name),
+            stars_only = TRUE
+          )
         )
-      )
-    ) %>%
-    return()
+    },
+    silent = TRUE
+  )
+  return(res)
 }
