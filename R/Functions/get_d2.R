8bb89b076490c1ac6dec128efd942bd53631a5f3
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-02-16 09:46:10 +0100

    in d2 est, return 1 for test of same models

--- R/Functions/get_d2.R
+++ R/Functions/get_d2.R
@@ -5,8 +5,15 @@ get_d2 <- function(data_source, null_model) {
       null_dev <- insight::get_deviance(null_model)
       mod_dev <- insight::get_deviance(data_source)
 
-      res <-
-        (null_dev - mod_dev) / null_dev
+      # in case models are the same
+      if (
+        null_dev == mod_dev
+      ) {
+        res <- 1
+      } else {
+        res <-
+          (null_dev - mod_dev) / null_dev
+      }
     },
     silent = TRUE
   )
