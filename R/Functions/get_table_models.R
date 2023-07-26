174181750e6ec92e45a59fa1080b96d6f83e49e8
OndrejMottl <ondrej.mottl@gmail.com>
2023-07-26 13:51:17 +0200

    get_table_models: get model formula

--- R/Functions/get_table_models.R
+++ R/Functions/get_table_models.R
@@ -1,6 +1,13 @@
 get_table_models <- function(data_source, ...) {
   data_source %>%
-      tidyr::unnest(test_to_null) %>%
-      get_cols_for_model_table(., ...)  %>% 
-      round_all_numbers()
+    dplyr::mutate(
+      mod_formula = purrr::map_chr(
+        .x = mod,
+        .f = ~ insight::find_formula(.x) %>%
+          as.character()
+      )
+    ) %>%
+    tidyr::unnest(test_to_null) %>%
+    get_cols_for_model_table(., ...) %>%
+    round_all_numbers()
 }
