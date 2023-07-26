4af2d9fc4db9e234d3615f2773cf38ace56ee4f7
OndrejMottl <ondrej.mottl@gmail.com>
2023-07-26 13:51:26 +0200

    get_cols_for_model_table: add model formula

--- R/Functions/get_cols_for_model_table.R
+++ R/Functions/get_cols_for_model_table.R
@@ -7,7 +7,8 @@ get_cols_for_model_table <- function(
       dplyr::any_of(
         c(
           add_first_cols,
-          "mod_name", "AICc", "delta",
+          "mod_name", "mod_formula",
+          "AICc", "delta",
           "model_df", "deviance", "d2",
           "residual_df",
           "lr_chisq", "chisq_df", "p_value_chisq", "lr_signif",
